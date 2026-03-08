130bae75df5238fdd5993c2da53f13c8ffeffa32
Frida Ramsing <eaa25flbr@students.eaaa.dk>
2026-03-08 15:47:46 +0100

    Added in the routes for the nav bare

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,14 @@ export default function App() {
     <>
 
       <Routes>
+        <Route path="/products" element={<Products />} />
+        <Route path="/mood" element={<div>Mood page</div>} />
+        <Route path="/women" element={<div>Woman page</div>} />
+        <Route path="/men" element={<div>Men page</div>} />
+        <Route path="/designers" element={<div>Designers page</div>} />
+        <Route path="/inspo" element={<div>Inspo page</div>} />
+        <Route path="/sale" element={<div>Sale page</div>} />
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/products/*" element={<Products />} />
       </Routes>
     </>
   );
