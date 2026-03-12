782ff9680f7e2cfe30914c9ea61fdf98803ae291
Frida Ramsing <eaa25flbr@students.eaaa.dk>
2026-03-12 11:47:00 +0100

    Test with Nanna

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import Products from "./pages/Products";
-import ProductDetail from "./pages/'ProductDetail"; 
+import ProductDetail from "./pages/ProductDetail";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/products" element={<Products />} />
-        <Route path="/products/:ProductId" element={<ProductDetail/>} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
         <Route path="/mood" element={<div>Mood page</div>} />
-        <Route path="/women" element={<div>Woman page</div>} />
-        <Route path="/men" element={<div>Men page</div>} />
+        <Route path="/women" element={<Products />} />
+        <Route path="/men" element={<Products />} />
         <Route path="/designers" element={<div>Designers page</div>} />
         <Route path="/inspo" element={<div>Inspo page</div>} />
         <Route path="/sale" element={<div>Sale page</div>} />
